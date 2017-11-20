907553ee3f42945e7e2b16c98f67cc2b4819540b
Cemal Erdemir <cerdemir@gmail.com>
2017-11-20 17:11:52 +0300

    started tests

--- src/EasyMapper.test/EasyMapperTest.cs
+++ src/EasyMapper.test/EasyMapperTest.cs
@@ -1,10 +1,66 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EasyMapper.test
 {
+    [TestClass]
     public class EasyMapperTest
     {
+
+        [TestMethod]
+        public void SetItemFromRow_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void SetItemToRow_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateItemFromRow_Test()
+        {
+        }
+
+        [TestMethod]
+        public void CreateListFromTable_Test()
+        {
+        }
+
+        [TestMethod]
+        public void CreateDataTable_Test()
+        {
+            var mapper = new EasyMapper();
+            var testData = GetTestEmployees();
+            var datatable = mapper.CreateDataTable(testData);
+
+            Assert.IsTrue(datatable.Columns.Contains("Id"));
+        }
+
+        private List<Employee> GetTestEmployees()
+        {
+            var testData = new Employee[] {
+                new Employee {
+                    Id =1,
+                    Married = true,
+                    Name = "Cemal",
+                    Surname = "Erdemir",
+                    Salary = 100
+                },
+                new Employee {
+                    Id =2,
+                    Married = false,
+                    Name = "İrfan",
+                    Surname = "Sağır",
+                    Salary = 300
+                }
+            };
+            return testData.ToList();
+        }
     }
 }
---
+++ src/EasyMapper.test/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyMapper.test
+{
+    class Employee
+    {
+        [MappingOrder(0)]
+        public int Id { get; set; }
+
+        [MappingOrder(1)]
+        public string Name { get; set; }
+
+        [MappingOrder(2)]
+        public string Surname { get; set; }
+
+        [MappingOrder(5)]
+        public decimal Salary { get; set; }
+
+       
+        [MappingOrder(4)]
+        public bool Married { get; set; }      
+      
+
+        [ExcludeMapping]
+        public string FullName
+        {
+            get
+            {
+                return $"{Name} {Surname}";
+            }
+        }
+
+    }
+}
